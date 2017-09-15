56d2492752248cd351ca3fabeb1b29665f35cd9c
Luan Sena <luansena@live.com>
2017-09-15 14:05:36 -0300

    set log_config to False

--- project/run.py
+++ project/run.py
@@ -5,7 +5,7 @@ from apps import sanic_app
 if __name__ == "__main__":
     try:
         app = sanic_app.get_app()
-        app.run(host="0.0.0.0", port=8888, debug=False)
+        app.run(host="0.0.0.0", port=8888, debug=False, log_config=None)
     except Exception as error:
         sys.stderr.write("Error while starting application: %s" % error)
         raise error
