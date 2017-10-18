b5fbd2a898788d3b50f750ddf675c99e6aa78be2
Luan Sena <lbarbosa@stone.com.br>
2017-10-18 14:47:36 -0200

    adds login url and verson to all urls

--- project/apps/person/urls.py
+++ project/apps/person/urls.py
@@ -1,14 +1,16 @@
 from sanic import Blueprint
 
+from apps.person.controller.person_login import PersonLogin
 from project.apps.person.controller.person import Person, PersonInstance
 
 
-class ProfessionalBP:
+class PersonBP:
     def __init__(self, db_conn):
         blueprint = Blueprint('person')
 
-        blueprint.add_route(Person.as_view(db_conn), '/person')
-        blueprint.add_route(PersonInstance.as_view(db_conn), '/person/<prof_id>')
+        blueprint.add_route(Person.as_view(db_conn), '/v1/person')
+        blueprint.add_route(PersonInstance.as_view(db_conn), '/v1/person/<prof_id>')
+        blueprint.add_route(PersonLogin.as_view(db_conn), '/v1/login')
 
         self.blueprint = blueprint
 
