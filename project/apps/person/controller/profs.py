1defab67518e5ed1f5fa3bb09d7950e7db207557
Luan Sena <luansena@live.com>
2017-11-25 19:31:15 -0200

    adds post ad profs.py

--- project/apps/person/controller/profs.py
+++ project/apps/person/controller/profs.py
@@ -1,4 +1,4 @@
-from sanic.response import json
+from sanic.response import json, text
 from sanic.views import HTTPMethodView
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -8,6 +8,27 @@ from project.database import db_request_manager
 class Prof(HTTPMethodView):
     def __init__(self, db_conn):
         self.db_conn = db_conn
+
+    async def options(self, request):
+        return text("ok")
+
+    async def post(self, request):
+        try:
+            request = request.json
+            school_id = request['schoolId']
+            person_id = request['personId']
+            db_request_manager.insert_prof(self.db_conn, school_id, person_id)
+            response = {"success": True}
+
+            return json(response, 202)
+        except Exception as e:
+            print(str(e))
+            return json({"success": False,
+                         "message": "unexpected error has occurred"}, 500)
+
+    async def get(self, request, school_id=None):
+        classes = db_request_manager.get_classes_by_school(self.db_conn, school_id)
+        return json(classes, 200)
 
     async def get(self, request, school_id):
         try:
