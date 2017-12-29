5660a0868f06075cbc97a6b9cc26bcf261bb07e1
Luan Sena <luancoudray@gmail.com>
2017-12-29 20:43:25 +0000

    remove log_config

--- project/run.py
+++ project/run.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     try:
         db_connection = driver_db_conn.get_session()
         app = sanic_app.get_app(db_connection)
-        app.run(host="0.0.0.0", port=8888, debug=False, log_config=None)
+        app.run(host="0.0.0.0", port=8888, debug=False)
     except Exception as error:
         sys.stderr.write("Error while starting application: %s" % error)
         raise error
